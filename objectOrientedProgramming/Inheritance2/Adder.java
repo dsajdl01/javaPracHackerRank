21f24bcdb8dfbdeac5169407a836fcb93764e0d3
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-26 12:17:05 +0100

    adding Adder class

---
+++ objectOrientedProgramming/Inheritance2/Adder.java
@@ -0,0 +1,14 @@
+
+/**
+ * class Adder
+ * 
+ * @author David Sajd 
+ * @version 26/08/2015
+ */
+public class Adder extends Arithmetic {
+   
+   @Override
+   public int add(int a, int b){
+        return a+b;
+    }
+}
