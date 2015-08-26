a0ab036732b9323399b06d32cd54c111e51a5433
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-26 10:15:15 +0100

    adding SolutionInharitance1 class

---
+++ objectOrientedProgramming/Inheritance1/SolutionInharitance1.java
@@ -0,0 +1,17 @@
+/**
+ *  class SolutionInharitence1
+ * 
+ * @author David Sajd
+ * @version 26/08/2015
+ */
+public class SolutionInharitance1 {
+    public static void main(String args[])
+   {
+
+     Bird bird = new Bird();
+     bird.walk();
+     bird.fly();
+     bird.sing();
+   
+   }
+}
