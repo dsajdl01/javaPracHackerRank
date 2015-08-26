3cc831b7efa6c6fcbbcedd612b735a685cf6f10a
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-26 16:11:05 +0100

    adding AdvancedArithmetic class

---
+++ objectOrientedProgramming/Interface/AdvancedArithmetic.java
@@ -0,0 +1,10 @@
+
+/**
+ * interface AdvancedArithmetic
+ * 
+ * @author David Sajd 
+ * @version 26/08/2015
+ */
+public interface AdvancedArithmetic {
+    public abstract int divisorSum(int n);
+}
