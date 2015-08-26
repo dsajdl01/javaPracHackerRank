d2b5e0a285b16ee5275d758f2f7f188b07c4719f
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-26 10:06:34 +0100

    adding Animal abstract class

---
+++ objectOrientedProgramming/Inheritance1/Animal.java
@@ -0,0 +1,14 @@
+
+/**
+ * class Animal here.
+ * 
+ * @author David Sajdl 
+ * @version 26/08/2015
+ */
+public class Animal {
+   
+    public void walk(){
+    
+      System.out.println("I am walking");
+    }
+}
