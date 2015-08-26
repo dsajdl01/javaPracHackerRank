24180e81e071f18a9eefc6327eb647b7ff307f9d
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-26 10:13:48 +0100

    adding Bird class

---
+++ objectOrientedProgramming/Inheritance1/Bird.java
@@ -0,0 +1,17 @@
+
+/**
+ * class Bird.
+ * 
+ * @author David Sajdl 
+ * @version 26/08/2015
+ */
+public class Bird extends Animal {
+    
+    public void fly(){
+        System.out.println("I am flying");
+    }
+   
+    public void sing(){
+        System.out.println("I am singing");
+    }
+}
