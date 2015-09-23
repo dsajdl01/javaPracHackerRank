ddaf02c7ff995194d6c44d5148aaaba83fb63022
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-09-23 22:47:41 +0100

    adding MyBook class

---
+++ objectOrientedProgramming/Abstract/MyBook.java
@@ -0,0 +1,15 @@
+package OOP;
+
+
+/**
+ * class MyBook.
+ * 
+ * @author David Sajd 
+ * @version 23/09/2015
+ */
+public class MyBook extends Book {
+
+    public void setTitle(String t){
+        title = t;
+    }
+}
