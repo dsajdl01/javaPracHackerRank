6163b60fa58b93f12d4839f631cec5ca627338f4
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-09-24 17:25:52 +0100

    adding Sports class

---
+++ objectOrientedProgramming/Overriding_1/Sports.java
@@ -0,0 +1,20 @@
+package OOP;
+
+
+/**
+ * Class Sports here.
+ * 
+ * @author David Sajdl 
+ * @version 24/09/2015
+ */
+public class Sports {
+   
+    String get_name(){
+        return "Generic Sports";
+   }
+    
+    void get_number_of_team_members() {
+      System.out.println("Each team has n players in "+get_name());
+   }
+    
+}
