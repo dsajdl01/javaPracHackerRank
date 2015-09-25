38098cbb2caa4a26023a76176d65e8fa6f79ab78
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-09-25 18:34:35 +0100

    adding Solution class

---
+++ objectOrientedProgramming/Overriding_2/Solution.java
@@ -0,0 +1,16 @@
+package OOP;
+
+
+/**
+ * Class Solution.
+ * 
+ * @author David Sajdl 
+ * @version 25/09/2015
+ */
+public class Solution {
+ 
+    public static void main(String[]args){
+        
+        MotorCycle M=new MotorCycle();
+    }
+}
