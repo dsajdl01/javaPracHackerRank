e672032b03a2694fc3e69e91e5f76c597e692c40
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-09-25 17:28:00 +0100

    adding BiCycle class

---
+++ objectOrientedProgramming/Overriding_2/BiCycle.java
@@ -0,0 +1,16 @@
+package OOP;
+
+
+/**
+ * Class BiCycle.
+ * 
+ * @author David Sajdl 
+ * @version 25/09/2015
+ */
+public class BiCycle {
+    
+   String define_me(){
+      return "a vehicel with pedals.";
+   }
+   
+}
